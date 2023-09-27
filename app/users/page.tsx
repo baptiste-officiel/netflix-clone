5ce049bd573150ad988e57ea5344f02dfc90e057
Baptiste <baptiste.lejeune@hotmail.com>
2023-09-27 12:46:33 +0200

    🐛 FIX: user type

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -21,7 +21,7 @@ async function page() {
       <div className='pt-22 bg-black min-h-screen text-white flex flex-col gap-8 justify-center items-center'>
         <h1 className="text-3xl md:text-5xl md:mb-8">Who is watching ?</h1>
         <div className="w-100 lg:w-4/5 mx-auto flex justify-center items-center gap-4">
-        <User name={data?.user?.name} img={data?.user?.image} />
+        <User name={data?.user?.name as string} img={data?.user?.image as string} />
         </div>
       </div>
   )
